022aafe552d575f2d8273a5e8bff2ee33ea70834
Briano Santos <brianosantos@gmail.com>
2016-08-31 21:03:38 -0700

    created 5 IBOutlets and 2 IBActions.  Connected them to the view controller.

--- Multiples4/Multiples4/ViewController.swift
+++ Multiples4/Multiples4/ViewController.swift
@@ -11,9 +11,34 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    //Properties
+    var multiple = 0
     
     
+    //Outlets
+    @IBOutlet weak var multiplesLogo: UIImageView!
+    @IBOutlet weak var multipleToAddByTextFld: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
     
+    @IBOutlet weak var pressAddToAddLabel: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    @IBAction func onPlayBtnPressed(sender: AnyObject) {
+        if multipleToAddByTextFld.text != nil && multipleToAddByTextFld != "" {
+            
+            multiplesLogo.hidden = true
+            multipleToAddByTextFld.hidden = true
+            playBtn.hidden = true
+            
+            pressAddToAddLabel.hidden = false
+            addBtn.hidden = false
+            
+            
+        }
+    }
+    
+    @IBAction func onAddBtnPressed(sender: AnyObject) {
+    }
     
     
     
