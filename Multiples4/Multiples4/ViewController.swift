639579b7dcc48c682f418f8ad470f34c2e2623bc
Briano Santos <brianosantos@gmail.com>
2016-08-31 22:45:16 -0700

    created 2 functions: isGameOver and restartGame.  Also, pasted in StackExchange func that removes keyboard from view when screen is tapped. Known Issues: the code in the onAddBtnPressed func is incomplete resulting in the wrong sum when the add button is pressed.

--- Multiples4/Multiples4/ViewController.swift
+++ Multiples4/Multiples4/ViewController.swift
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
     
     //Properties
+    var numA = 0
     var multiple = 0
+    var sum = 0
+    
+    var maxNumber = 20
+   
     
     
     //Outlets
@@ -24,7 +29,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var addBtn: UIButton!
     
     @IBAction func onPlayBtnPressed(sender: AnyObject) {
-        if multipleToAddByTextFld.text != nil && multipleToAddByTextFld != "" {
+        if multipleToAddByTextFld.text != nil && multipleToAddByTextFld.text != "" {
             
             multiplesLogo.hidden = true
             multipleToAddByTextFld.hidden = true
@@ -33,18 +38,63 @@ class ViewController: UIViewController {
             pressAddToAddLabel.hidden = false
             addBtn.hidden = false
             
-            
+            multiple = Int(multipleToAddByTextFld.text!)!
         }
     }
     
     @IBAction func onAddBtnPressed(sender: AnyObject) {
+        
+        sum = numA + multiple
+        numA = sum
+        
+        pressAddToAddLabel.text = "\(numA) + \(multiple) = \(sum)"
+        
+        isGameOver()
+    }
+    
+    func isGameOver() {
+        if sum >= maxNumber {
+           restartGame()
+        }
+    }
+    
+    func restartGame() {
+        
+        multiplesLogo.hidden = false
+        multipleToAddByTextFld.hidden = false
+        playBtn.hidden = false
+        
+        pressAddToAddLabel.hidden = true
+        addBtn.hidden = true
+        
+        multiple = 0
+        multipleToAddByTextFld.text = ""
+        
     }
     
     
     
     
     
-
-
+    // BEGIN stachExchange code (cut/paste) to make the keyboard dissappear when the screen is tapped https://stackoverflow.com/questions/24126678/close-ios-keyboard-by-touching-anywhere-using-swift
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+        // END stachExchange code
+    
+    
+    
+    
+    
 }
-
